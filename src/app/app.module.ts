f3d61a1992a612345b9009b7d490661c0975d401
Stephen <stephen@vindevcorp.com>
2022-04-26 09:31:49 -0500

    adding additional scully files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StuffComponent } from './stuff/stuff.component';
 import { HomeComponent } from './home/home.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StuffComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, StuffComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, ScullyLibModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
